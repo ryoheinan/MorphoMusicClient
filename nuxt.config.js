aa8f393f936d792a55b6c2af1cdb26e2f1a83ef7
aleeca <49315610+aleeca@users.noreply.github.com>
2021-03-21 00:55:31 +0900

    title変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,6 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Morphomusic',
     title: 'Morphomusic',
     htmlAttrs: {
       lang: 'ja',
