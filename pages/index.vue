fbc80c6073805f58ac393055f8113457aa1554c7
aleeca <49315610+aleeca@users.noreply.github.com>
2021-03-21 00:47:01 +0900

    画像の追加

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6" class="text-center">
       <div class="text-center mb-5">
+        <v-avatar size="200" tile>
+          <img src="~/assets/img/logo.jpg" />
+        </v-avatar>
         <h1>Morphomusic</h1>
         <p>あなたの音楽の幅を広げるLINE Bot</p>
         <div
