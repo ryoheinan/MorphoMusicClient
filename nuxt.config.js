e0b34660c3ee9afac17f83e7300ac8816512b836
aleeca <49315610+aleeca@users.noreply.github.com>
2021-03-21 00:47:13 +0900

    不要なファイル等の削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,3 @@
-// import colors from 'vuetify/es5/util/colors'
-import path from 'path'
-import fs from 'fs'
 require('dotenv').config()
 const { LIFFID } = process.env
 
@@ -64,19 +61,6 @@ export default {
     customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: false,
-      /*
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-      */
     },
   },
 
@@ -87,17 +71,5 @@ export default {
         fs: 'empty',
       }
     },
-  },
-
-  server: {
-    port: 3000,
-    host: 'localhost',
-    https:
-      process.env.NODE_ENV === 'production'
-        ? {}
-        : {
-            key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-            cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-          },
   },
 }
